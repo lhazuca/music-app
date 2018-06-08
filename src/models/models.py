18d24939e1ce15a5ae7af61800bbd79386fc6687
Brian Goldman <leonel89011@gmail.com>
2018-06-08 13:20:45 -0300

    Basic playlist finished with merge

--- src/models/models.py
+++ src/models/models.py
@@ -17,19 +17,12 @@ class Playlist(Base):
     userName = Column(String(40), ForeignKey('User_Data.userName', ondelete='CASCADE'), primary_key=True)
     description = Column(String(100))
 
-# class AudioFileByPlaylist(Base):
-#     __tablename__ = 'AudioFileByPlaylist'
-#     filename = Column(String(40), ForeignKey('AudioFile.filename', ondelete='CASCADE'), primary_key=True)
-#     playlistName = Column(String(50), ForeignKey('Playlist.playlistName', ondelete='CASCADE'), primary_key=True)
-#     created = Column(DateTime, default=func.now())
-
 class User_Data(Base):
     __tablename__ = 'User_Data'
     userName = Column(String(50), primary_key=True)
     name = Column(String(40))
     lastName = Column(String(40))
     age = Column(Integer())
-
 
 class User_Login(Base):
     __tablename__ = 'User_Login'
